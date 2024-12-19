6b444b5a278092fd9c492123a1ac381722a491af
Imdadul Haque Ohi <imdadul0202@gmail.com>
2024-12-20 01:01:03 +0600

    update auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,7 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  refreshAccessToken,
 } = require("../controllers/user.controller");
 
 const { verifyJWT } = require("../middlewares/auth.middleware");
@@ -11,5 +12,9 @@ const { verifyJWT } = require("../middlewares/auth.middleware");
 router.post("/registerUser", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", verifyJWT, logoutUser);
+router.post("/refresh-token", refreshAccessToken);
+
+// forgot-password
+// reset-password
 
 module.exports = router;
